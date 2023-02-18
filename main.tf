211fdd371c61f5887fa0a9882a5fc96014f8d6db
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-18 16:42:32 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -56,6 +56,14 @@ resource "aws_security_group" "main" {
 
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
 
   tags = {
     Name = "roboshop-${var.env}-redis"
